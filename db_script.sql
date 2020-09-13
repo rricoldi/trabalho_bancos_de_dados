343ec2e0ea94f6cd7a04840d48e811b3d2fa362b
RenanRicoldi <renan.ricoldi.frois@gmail.com>
2020-09-13 11:16:55 -0300

    restrições not null inseridas

--- db_script.sql
+++ db_script.sql
@@ -2,8 +2,8 @@ create schema trabalho;
 
 create table trabalho.podcast(
 	rss_feed char(200),
-    nome char(100),
-    site char(200),
+    nome char(100) not null,
+    site char(200) not null,
     constraint pk_podcast PRIMARY KEY(rss_feed)
 );
 
@@ -18,16 +18,16 @@ create table trabalho.tags_podcast(
 
 create table trabalho.usuario(
 	email char(100),
-    nome char(100),
-    sexo char(100),
-    idade int,
-    senha char(30),
-    pais char(50),
+    nome char(100) not null,
+    sexo char(100) not null,
+    idade int not null,
+    senha char(30) not null,
+    pais char(50) not null,
     constraint pk_usuario PRIMARY KEY(email)
 );
 
 create table trabalho.usuario_esta_inscrito_no_podcast(
-	classificacao int,
+	classificacao int not null,
     rss_feed char(200),
     email char(100),
     constraint pk_usuario_podcast PRIMARY KEY(email, rss_feed),
@@ -40,7 +40,7 @@ create table trabalho.usuario_esta_inscrito_no_podcast(
 );
 
 create table trabalho.comentarios_podcast_usuario(
-	comentario text,
+	comentario text not null,
     rss_feed char(200),
     email char(100),
     constraint pk_usuario_podcast PRIMARY KEY(email, rss_feed),
